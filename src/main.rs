f32696f8317738a0af16ce4414939bae2a158d65
Ziqul <m.s.v.00a@gmail.com>
2025-10-30 10:58:49 +0100

    Added small system to draw coordinates on screen

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,17 @@ fn main() {
         .add_systems(OnEnter(globals::AppState::Game), |mut next: ResMut<NextState<globals::AppState>>| next.set(globals::AppState::Pause))
         .add_systems(OnEnter(globals::AppState::Pause), |mut next: ResMut<NextState<globals::AppState>>| next.set(globals::AppState::End))
         .add_systems(OnEnter(globals::AppState::End), exit_app)
+//        .add_systems(Update, draw_coordinates)
         .run();
 }
 
 fn exit_app(mut exit: MessageWriter<AppExit>) {
     exit.write(AppExit::Success);
+}
+
+fn draw_coordinates(mut gizmos: Gizmos) {
+    // Draw axes centered at origin
+    gizmos.line(Vec3::ZERO, Vec3::X + 1000.0, Color::srgb(1., 0., 0.)); // X-axis
+    gizmos.line(Vec3::ZERO, Vec3::Y * 1000.0, Color::srgb(0., 1., 0.)); // Y-axis
+    gizmos.line(Vec3::ZERO, Vec3::Z * 1000.0, Color::srgb(0., 0., 1.)); // Z-axis
 }
